c43ac05eddf091cb2a69136ee7c69653b0d8656e
mnburkle <mnburkle@stanford.edu>
2014-02-05 16:20:06 -0800

    fixed the putting it together lab which was went VERY POORLY BTW

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -10,11 +10,16 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Javascript connected!");
+	$("a.friend-name").click(function(e) {
+		e.preventDefault();
+		var myname = $(this).text();
+		var anagram = anagrammedName(myname);
+		$(this).text(anagram);
+	});
 }
 
 function anagrammedName(name) {
 	// Thanks, Internet Anagram Server!
-	
 	if (name == "Doug Engelbart") {
 		return "Notable Grudge";
 	} 
